6de0b889f584d03e896ca9f97a87f6f132a90eba
Margo Urey <margo@margonline.co.uk>
2014-10-10 18:11:49 +0100

    add gmapsforrails, underscore-rails

--- Gemfile
+++ Gemfile
@@ -9,12 +9,14 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'envyable'
 gem 'foursquare2'
 gem 'geocoder'
+gem 'gmaps4rails'
 gem 'jquery-rails', '~> 4.0.0.beta2'
 gem 'jbuilder', '~> 2.0'
 gem 'rails-html-sanitizer'
 gem 'turbolinks'
 gem 'sass-rails', '~> 5.0.0.beta1'
 gem 'uglifier', '>= 1.3.0'
+gem 'underscore-rails'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,6 +8,7 @@ class MainController < ApplicationController
       @venues = @fsquare_data.venues.map do |venue|
         Venue.new(venue)
       end
+      @map_markers = get_coords_for(@venues)
       render 'venues'
     else
       errors.add(:base, 'this is not a valid postcode')
@@ -17,15 +18,20 @@ class MainController < ApplicationController
   private
 
   def valid_postcode?(postcode)
-    # postcode = postcode.replace(/\s/g, "")
-    # regex = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i
-    # regex.test(postcode) ? true : false
-    true
+    regex = "/^([A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]|[A-HK-Y][0-9]([0-9]|[ABEHMNPRV-Y]))|[0-9][A-HJKS-UW])\s?[0-9][ABD-HJLNP-UW-Z]{2}|(GIR\ 0AA)|(SAN\ TA1)|(BFPO\ (C\/O\ )?[0-9]{1,4})|((ASCN|BBND|[BFS]IQQ|PCRN|STHL|TDCU|TKCA)\ 1ZZ))$$/i"
+    postcode =~ regex ? true : false
   end
 
   def get_venues(postcode)
     coordinates = Geocoder.coordinates(postcode)
     FoursquareService.new(coordinates[0], coordinates[1]).search_venues
+  end
+
+  def get_coords_for(venues)
+    Gmaps4rails.build_markers(venues) do | venue, marker|
+      marker.lat venue.location.lat
+      marker.long venue.location.long
+    end
   end
 
 end
