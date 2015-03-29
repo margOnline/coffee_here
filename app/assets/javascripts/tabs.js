46759364a33bf9841c31ed8b4dac4d2d7321b61f
Margo Urey <margo@margonline.co.uk>
2015-03-29 19:16:03 +0100

    add id to panels

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,18 +1,18 @@
-// $('.nav-tabs').each(function(){
-//   var $this = $(this);
-//   var $tab = $this.find('li.active');
-//   var $link = $tab.find('a');
-//   var $panel = $($link.attr('href'));
+$('.nav-tabs').each(function(){
+  var $this = $(this);
+  var $tab = $this.find('li.active');
+  var $link = $tab.find('a');
+  var $panel = $($link.attr('href');
 
-//   $this.on('click', function(e){
-//     e.preventDefault();
-//     var $link = $this;
-//     var id = $this.hash;
-//     if (id && !$link.is('.active')) {
-//       $tab.removeClass('.active');
-//       $panel.removeClass('.active');
-//       $tab = $link.parent().addClass('active');
-//       $panel = $(id).addClass('active');
-//     }
-//   });
-// });
+  $this.on('click', function(e){
+    e.preventDefault();
+    var $link = $this;
+    var id = $this.hash;
+    if (id && !$link.is('.active')) {
+      $tab.removeClass('.active');
+      $panel.removeClass('.active');
+      $tab = $link.parent().addClass('active');
+      $panel = $(id).addClass('active')
+    }
+  });
+});
