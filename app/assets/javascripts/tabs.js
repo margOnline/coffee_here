44c14338795b2c66059e313392ab6eee06c174b7
Margo Urey <margo@margonline.co.uk>
2015-03-30 14:22:39 +0100

    try to get panel view working

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,18 +1,19 @@
-$('.nav-tabs').each(function(){
+$('.tab-list').each(function(){
   var $this = $(this);
   var $tab = $this.find('li.active');
   var $link = $tab.find('a');
-  var $panel = $($link.attr('href');
+  var $panel = $($link.attr('href'));
 
-  $this.on('click', function(e){
+  $this.on('click', '.tab-control', function(e){
     e.preventDefault();
-    var $link = $this;
-    var id = $this.hash;
+    var $link = $(this);
+    var id = this.hash;
     if (id && !$link.is('.active')) {
-      $tab.removeClass('.active');
-      $panel.removeClass('.active');
-      $tab = $link.parent().addClass('active');
-      $panel = $(id).addClass('active')
+      $panel.removeClass('active');
+      $tab.removeClass('active');
+
+      $panel = $(id).addClass('active');
+      $tab = $link.parent().addClass('active');   
     }
   });
 });
