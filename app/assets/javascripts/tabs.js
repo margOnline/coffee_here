2ffae48b44e08775a8f7ee3fc0452e8da23437fa
Margo Urey <margo@margonline.co.uk>
2015-04-05 17:31:41 +0100

    add document.ready to tabs

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,33 +1,21 @@
+$(function(){
+  $('.nav-tabs').each(function(){
+    var $this = $(this);
+    var $tab = $this.find('li.active');
+    var $link = $tab.find('a');
+    var $panel = $($link.attr('href'));
 
-console.log($('.tab-list'));
+    $this.on('click', '.tab-control', function(e){
+      e.preventDefault();
+      var $link = $(this);
+      var id = this.hash;
+      if (id && !$link.is('.active')) {
+        $panel.removeClass('active');
+        $tab.removeClass('active');
 
-
-$('.tab-list a').on('click', function() {
-  var link = $(this).attr('href');
-  console.log(link);
-  // $(link)
-});
-
-
-$('.tab-list').each(function(){
-  console.log($(this));
-  var $this = $(this);
-  var $tab = $this.find('li.active');
-  var $link = $tab.find('a');
-  var $panel = $($link.attr('href'));
-
-  $this.on('click', '.tab-control', function(e){
-    e.preventDefault();
-    var $link = $(this);
-    var id = this.hash;
-    console.log($link);
-    console.log(id);
-    if (id && !$link.is('.active')) {
-      $panel.removeClass('active');
-      $tab.removeClass('active');
-
-      $panel = $(id).addClass('active');
-      $tab = $link.parent().addClass('active');   
-    }
+        $panel = $(id).addClass('active');
+        $tab = $link.parent().addClass('active');   
+      }
+    });
   });
 });
